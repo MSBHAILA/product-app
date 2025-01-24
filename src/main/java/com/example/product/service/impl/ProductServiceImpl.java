24808ca51935ad696a2bf38f6b82a429d8ee0932
Mohammed Bhaila <mohammed.bhaila@riaadvisory.com>
2025-01-25 00:36:35 +0300

    Enable caching for ProductApplication.

--- src/main/java/com/example/product/service/impl/ProductServiceImpl.java
+++ src/main/java/com/example/product/service/impl/ProductServiceImpl.java
@@ -5,12 +5,17 @@ import com.example.product.dto.ProductDto;
 import com.example.product.entity.Product;
 import com.example.product.mapper.ProductMapper;
 import com.example.product.service.ProductService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.CollectionUtils;
 
 import java.util.List;
 
+@Slf4j
 @Service
 public class ProductServiceImpl implements ProductService {
 
@@ -21,8 +26,10 @@ public class ProductServiceImpl implements ProductService {
     ProductRepository productRepository;
 
     @Override
+    @Cacheable(cacheNames = "products", key = "#start")
     public List<ProductDto> getProducts(int start) {
         List<ProductDto> products = productMapper.productListToProductDtoList(productRepository.getProducts(start));
+        log.info("getProducts | fetch data from db");
         if (!CollectionUtils.isEmpty(products)) {
             return products;
         }
@@ -38,6 +45,7 @@ public class ProductServiceImpl implements ProductService {
     }
 
     @Override
+    @CacheEvict(cacheNames = "products", allEntries = true)
     public ProductDto updateProduct(ProductDto productDto) {
         if (null != productDto && productRepository.existsById(productDto.getProductId())) {
             return productMapper.productToProductDto(productRepository.save(productMapper.productDtoToProduct(productDto)));
@@ -46,6 +54,7 @@ public class ProductServiceImpl implements ProductService {
     }
 
     @Override
+    @CacheEvict(cacheNames = "products", key = "#start")
     public boolean deleteProducts(int start) {
         List<Product> products = productRepository.deleteProducts(start);
         return !CollectionUtils.isEmpty(products);
